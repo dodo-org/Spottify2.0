1a6c0ba49475abcf4d373773e86b98ee83335ae3
dennis <dennis.krischal@fh-erfurt.de>
2024-08-17 10:13:59 +0200

    Deleted more prjkt files

--- FinalProject/Spotify_Api/Models/Request/RegisterRequest_Model.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Spotify_Api.Models.Request
-{
-    public class RegisterRequest_Model
-    {
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
