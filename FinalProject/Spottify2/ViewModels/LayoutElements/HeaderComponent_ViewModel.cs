c7052e1dddb1f75dcbbd4818b022cd0e436e05f8
Dominic0074 <61750782+Dominic0074@users.noreply.github.com>
2024-08-18 20:29:59 +0200

    Add Viewmodels

---
+++ FinalProject/Spottify2/ViewModels/LayoutElements/HeaderComponent_ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spottify2.ViewModels.LayoutElements
+{
+    class HeaderComponent_ViewModel
+    {
+    }
+}
